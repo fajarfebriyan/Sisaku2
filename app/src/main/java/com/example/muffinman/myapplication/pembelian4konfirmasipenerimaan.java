a1dc3a40b0fee241fced155f1cdd72b22b1f9242
fajarfebriyan <fajar12345>
2016-02-17 19:01:17 +0700

    nambah lagi hehe

---
+++ app/src/main/java/com/example/muffinman/myapplication/pembelian4konfirmasipenerimaan.java
@@ -0,0 +1,13 @@
+package com.example.muffinman.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pembelian4konfirmasipenerimaan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pembelian4konfirmasipenerimaan);
+    }
+}
